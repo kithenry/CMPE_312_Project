694449889a622f356ee51aa2a2d1562eee933ff7
kithenry <21903316@emu.edu.tr>
2025-05-13 06:35:00 +0400

    Add login.js to handle user authentication

--- frontend/src/js/pages/login.js
+++ frontend/src/js/pages/login.js
@@ -1,11 +1,27 @@
 // src/js/pages/login.js
 import { login } from '../api/auth.js';
+import { isAuthenticated } from '../api/auth.js';
 
 export default (app) => {
+    if (isAuthenticated()) {
+        window.location.href = '/home';
+        return;
+    }
+
     app.innerHTML = `
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
                 <a class="navbar-brand" href="/">Unibookswap</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="/register">Register</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/login">Login</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/search">Search</a></li>
+                    </ul>
+                </div>
             </div>
         </nav>
         <div class="container mt-5" style="max-width: 400px;">
